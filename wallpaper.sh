1bfdba24a9adc53b70014ea49f399ec155c5a28b
Antoine <antoine.morbois@gmail.com>
2019-10-09 07:53:51 +0200

    Correction du script

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,18 +1,17 @@
 #sudo apt install libxml2-utils
 
-wget -q -O - https://fuckinghomepage.com/ | xmllint --html --xpath 'html/body/div[2]/div[3]/div[1]/div/p[17]/small/a'
 a=$(wget -F -q -O - https://fuckinghomepage.com/ | xmllint --html --xpath '(//a)[9]/@href' - 2>/dev/null)
-a$
+
 b=$(echo $a | cut -c 7- | sed 's/.$//')
 b=${b//amp;}
 c=$(curl -L $b | xmllint --html --xpath '(//meta)[2]/@content' - 2>/dev/null)
 d=$( echo $c | cut -c 16- |  sed 's/.$//')
 
-wget --output-document $(date '+%Y-%m-%d')
-
 wallpaper="$(pwd)/$(date '+%Y-%m-%d')"
-rm /usr/share/backgrounds/linuxmint/default_background.jpg
-cp $wallpaper /usr/share/backgrounds/linuxmint/default_background.jpg
+wget --output-document $wallpaper $d
+
+sudo rm /usr/share/backgrounds/linuxmint/default_background.jpg
+sudo cp $wallpaper /usr/share/backgrounds/linuxmint/default_background.jpg
 
 
 ## TODO
